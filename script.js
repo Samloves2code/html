e938a12af54df2b0206bf1524ce9727ce0bf20a9
Samuel <80214737+Samuelcoder1@users.noreply.github.com>
2021-03-13 12:17:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -14,5 +14,4 @@ function myfunction(){
 		element.classList.add('animate');
 	}, 7000); //5000 = 5 second
 	
-	alert("Will spin when you press 'OK' / If you no longer want this to appear please: Turn pop-ups on or off / On your computer, open Chrome. / At the top right, click More More and then Settings. / Under Privacy and security, click Site settings. / Click Pop-ups and redirects.");
 }
